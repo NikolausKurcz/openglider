cbcdbb5a61e5852ca33ae790b15f6b1c90c27f2a
NikolausKurcz <nikolaus@kurcz.de>
2024-11-10 22:57:01 +0100

    minirib cutback

--- openglider/glider/parametric/table/cell/miniribs.py
+++ openglider/glider/parametric/table/cell/miniribs.py
@@ -3,16 +3,17 @@ from openglider.glider.parametric.table.base.dto import DTO
 from openglider.glider.rib import MiniRib
 
 from openglider.glider.parametric.table.base import CellTable
-from openglider.vector.unit import Percentage
+from openglider.vector.unit import Percentage, Length
 
 class MiniRibDTO(DTO):
     y_value: Percentage
     front_cut: Percentage
     back_cut: Percentage
+    TrailingEdgeCut: Length | Percentage
     material_code: str
 
     def get_object(self) -> Any:
-        return MiniRib(yvalue=self.y_value.si, front_cut=self.front_cut.si, back_cut=self.back_cut.si, material_code=self.material_code)
+        return MiniRib(yvalue=self.y_value.si, front_cut=self.front_cut.si, back_cut=self.back_cut.si, trailing_edge_cut=self.TrailingEdgeCut, material_code=self.material_code)
 
 class MiniRibTable(CellTable):
     dtos = {
