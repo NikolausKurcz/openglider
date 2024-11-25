5519af237d1d84c7e99f36a272047fd919ca159e
NikolausKurcz <nikolaus@kurcz.de>
2024-11-25 16:54:07 +0100

    snake case

--- openglider/glider/parametric/table/cell/miniribs.py
+++ openglider/glider/parametric/table/cell/miniribs.py
@@ -9,11 +9,11 @@ class MiniRibDTO(DTO):
     y_value: Percentage
     front_cut: Percentage
     back_cut: Percentage
-    TrailingEdgeCut: Length | Percentage
+    trailing_edge_cut: Length | Percentage
     material_code: str
 
     def get_object(self) -> Any:
-        return MiniRib(yvalue=self.y_value.si, front_cut=self.front_cut.si, back_cut=self.back_cut.si, trailing_edge_cut=self.TrailingEdgeCut, material_code=self.material_code)
+        return MiniRib(yvalue=self.y_value.si, front_cut=self.front_cut.si, back_cut=self.back_cut.si, trailing_edge_cut=self.trailing_edge_cut, material_code=self.material_code)
 
 class MiniRibTable(CellTable):
     dtos = {
